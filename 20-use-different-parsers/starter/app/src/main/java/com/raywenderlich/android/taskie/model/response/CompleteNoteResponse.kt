6b4240dc66e3302b7bd90a4f207fa2da64f9c0bc
Subhrajyoti Sen <subhrajyoti12@gmail.com>
2022-07-25 17:19:43 +0530

    Update Chapter 02

--- 20-use-different-parsers/starter/app/src/main/java/com/raywenderlich/android/taskie/model/response/CompleteNoteResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.raywenderlich.android.taskie.model.response
-
-import com.squareup.moshi.Json
-
-/**
- * Holds a message response after a note is completed.
- */
-class CompleteNoteResponse(@field:Json(name = "message") val message: String?)
