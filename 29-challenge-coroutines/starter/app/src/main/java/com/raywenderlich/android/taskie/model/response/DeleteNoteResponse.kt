9366e774fe8b838b41ed2f986e25509905e9c6b7
Subhrajyoti Sen <subhrajyoti12@gmail.com>
2022-08-04 04:27:07 +0530

    Update Chapter 11

--- 29-challenge-coroutines/starter/app/src/main/java/com/raywenderlich/android/taskie/model/response/DeleteNoteResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.raywenderlich.android.taskie.model.response
-
-import kotlinx.serialization.Serializable
-
-/**
- * Holds the message response from the delete note request.
- */
-@Serializable
-class DeleteNoteResponse(val message: String)
